e398df9fd04808015e119c7ab2ae2f8f0e650a4e
vishnubenny <vishnubenny@gmail.com>
2025-06-10 00:38:19 +0530

    Added common code architecture classes

--- shared/src/commonMain/kotlin/com/example/weatherapp/weather/data/fetcher/WeatherApiFetcher.kt
+++ shared/src/commonMain/kotlin/com/example/weatherapp/weather/data/fetcher/WeatherApiFetcher.kt
@@ -2,10 +2,8 @@ package com.example.weatherapp.weather.data.fetcher
 
 import com.example.weatherapp.weather.data.backend.WeatherBackend
 import com.example.weatherapp.weather.data.model.WeatherResponse
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 
 class WeatherApiFetcher(
     private val backend: WeatherBackend
@@ -13,5 +11,5 @@ class WeatherApiFetcher(
     fun getWeather(lat: Double, lon: Double): Flow<WeatherResponse> = flow {
         val response = backend.getWeatherRaw(lat, lon)
         emit(response)
-    }.flowOn(Dispatchers.Default)
+    }
 }
--- shared/src/commonMain/kotlin/com/example/weatherapp/weather/data/repository/WeatherRepository.kt
+++ shared/src/commonMain/kotlin/com/example/weatherapp/weather/data/repository/WeatherRepository.kt
@@ -1,7 +1,8 @@
 package com.example.weatherapp.weather.data.repository
 
-import com.example.weatherapp.weather.domain.model.WeatherViewState
+import com.example.weatherapp.weather.data.model.WeatherResponse
+import kotlinx.coroutines.flow.Flow
 
 interface WeatherRepository {
-    suspend fun getWeather(lat: Double, lon: Double): WeatherViewState
+    suspend fun getWeather(lat: Double, lon: Double): Flow<WeatherResponse>
 }
